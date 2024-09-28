87c4ecd77b8b0387d16e6548d86a3ec9c5d0a379
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-09-28 19:48:33 -0300

    Criando o arquivo responsável pelo gerenciamente de rotas

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import { routes } from './routes/routes'
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+// app.use(routes)
 
 export { app }
