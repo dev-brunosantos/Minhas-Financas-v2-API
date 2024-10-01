9e033110a854030ce107e0295ee61fadf1fbf4cc
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-10-01 07:14:53 -0300

    Adicionando o dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { app } from "./app"
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const { LINK, PORT } = process.env
 
 const link = LINK
-const port = PORT
+const port = PORT || 4000
 const url = `${link}:${port}`
 
 app.listen(port, () => console.log(`Servidor rodando em: ${url}`))
