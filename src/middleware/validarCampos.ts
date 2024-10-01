9b663deb62a51e7e0cb856b4170f273124834ce8
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-10-01 08:18:10 -0300

    Criando middleware para validar preenchimento de formulário

---
+++ src/middleware/validarCampos.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express'
+
+export default function ValidarCampos(req: Request, res: Response, next: NextFunction) {
+    const { nome, email, senha } = req.body
+    if(nome === "" || email === "" || senha === "") {
+        return res.json({ erro: "Todos os campos devem ser preenchidos."})
+    }
+    return next()
+}
