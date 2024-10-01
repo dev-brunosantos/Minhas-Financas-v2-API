44164f52418a3bdaa6dbf276bcc436bc8ac038d9
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-10-01 13:24:29 -0300

    Correção e melhoria no codigo

--- src/middleware/validarCampos.ts
+++ src/middleware/validarCampos.ts
@@ -3,8 +3,8 @@ import { Request, Response, NextFunction } from 'express'
 export default function ValidarCampos(req: Request, res: Response, next: NextFunction) {
     const { nome, email, senha } = req.body
     
-    if(nome !== "" || email !== "" || senha !== "") {
-        return next()
+    if(!nome.trim() || !email.trim() || !senha.trim()) {
+        return res.json({ erro: "Todos os campos devem ser preenchidos."})
     }
-    return res.json({ erro: "Todos os campos devem ser preenchidos."})
+    return next()
 }
