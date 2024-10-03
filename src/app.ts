8b2f8e1174123eb66688264e7b1dec0f545186cc
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-10-03 12:45:35 -0300

    Criando rotas para ter o controler das saidas

--- src/app.ts
+++ src/app.ts
@@ -9,5 +9,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 app.use(routes.UsuarioRoutes)
 app.use(routes.EntradasRoutes)
+app.use(routes.SaidasRoutes)
 
 export { app }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { EntradasRoutes } from "./entradas.routes"
+import { SaidasRoutes } from "./saidas.routes"
 import { UsuarioRoutes } from "./usuario.routes"
 
 const routes = {
     UsuarioRoutes,
-    EntradasRoutes
+    EntradasRoutes,
+    SaidasRoutes
 }
 
 export { routes }
