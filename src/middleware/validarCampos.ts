27ba369876699deafe6c50b356e13a977a4470d6
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-10-03 10:48:19 -0300

    Adicionando novas atualizações

--- src/middleware/validarCampos.ts
+++ src/middleware/validarCampos.ts
@@ -2,9 +2,13 @@ import { Request, Response, NextFunction } from 'express'
 
 export default function ValidarCampos(req: Request, res: Response, next: NextFunction) {
     const { nome, email, senha } = req.body
+    // const { titulo, descricao, valor } = req.body
     
-    if(!nome.trim() || !email.trim() || !senha.trim()) {
+    if(!nome.trim() || !email.trim() || !senha.trim() ) {
         return res.json({ erro: "Todos os campos devem ser preenchidos."})
     }
+    // if(!titulo.trim() || !descricao.trim() || !valor.trim() ) {
+    //     return res.json({ erro: "Todos os campos devem ser preenchidos."})
+    // }
     return next()
 }
